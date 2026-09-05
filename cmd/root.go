672ca622e3aac8b20b5e13b92ef8b54520aa7cda
olucasjose <65680721+olucasjose@users.noreply.github.com>
2026-09-05 20:28:53 -0300

    chore: bump CLI version to 7.12.0

--- cmd/root.go
+++ cmd/root.go
@@ -14,7 +14,7 @@ import (
 
 var rootCmd = &cobra.Command{
 	Use:           "tae",
-	Version:       "7.11.0",
+	Version:       "7.12.0",
 	Short:         "Tae é um utilitário CLI para extração e empacotamento de código",
 	SilenceErrors: true,
 	SilenceUsage:  true,
